882fbe5e0ead70af2f1d6b8f21055242a552ed02
ClarenceLuo <clarenceluoguancong@gmail.com>
2019-10-11 15:36:19 +0800

    git

--- iiwa_ros/src/tests_guancong/test_cartesian_guancong.cpp
+++ iiwa_ros/src/tests_guancong/test_cartesian_guancong.cpp
@@ -22,8 +22,7 @@ void signalHandler(int /*unused*/) { quit = true; }
 
 int main(int argc, char **argv)
 {
-
-// get roslauch pama
+  // get roslauch param
   ros::init(argc, argv, "show_param");
 
   ros::NodeHandle nh;
@@ -36,33 +35,23 @@ int main(int argc, char **argv)
   double rotation_z;
   double command_cartisian_mode;
   
-  nh.getParam("direction_x_add", direction_x_add);
-  nh.getParam("direction_y_add", direction_y_add);
-  nh.getParam("direction_z_add", direction_z_add);
+  nh.getParam("CartesianPoseState_1569786053275419313/direction_x_add", direction_x_add);
+  nh.getParam("CartesianPoseState_1569786053275419313/direction_y_add", direction_y_add);
+  nh.getParam("CartesianPoseState_1569786053275419313/direction_z_add", direction_z_add);
   nh.getParam("rotation_x", rotation_x);
   nh.getParam("rotation_y", rotation_y);
   nh.getParam("rotation_z", rotation_z);
   nh.getParam("command_cartisian_mode", command_cartisian_mode);
 
-  ROS_INFO("parameter is loading................. %f", direction_x_add);
-
-
+  ROS_INFO("parameter is finish load................. ");
 
 
   iiwa_ros::state::CartesianPose pose_state;
-  
-  // if (command_cartisian_mode==1)
-  // {
-    iiwa_ros::command::CartesianPose  pose_command;
-    pose_command.init("iiwa");
-  // }
-  // else
-  // {
-  //   iiwa_ros::command::CartesianPoseLinear  pose_command;
-  //   pose_command.init("iiwa");
-  // }
-  
+  iiwa_ros::command::CartesianPose  pose_command;
+  pose_command.init("iiwa");  
   pose_state.init("iiwa"); // That is the namespace under which the topic lives.
+
+  ROS_INFO("init is finish................. "); 
 
 
   // ROS spinner.
@@ -74,61 +63,60 @@ int main(int argc, char **argv)
   signal(SIGINT, signalHandler);
   signal(SIGHUP, signalHandler);
 
+  ROS_INFO("singal is finish................. "); 
   // Wait a bit, so that you can be sure the subscribers are connected.
   ros::Duration(0.5).sleep();
 
-  // while (!quit)
+
+  iiwa_msgs::CartesianPose car_pose = pose_state.getPose();
+  std::cout << "Pose: " << car_pose.poseStamped.pose.orientation.w << std::endl;
+
+  ROS_INFO_STREAM(
+    std::to_string(car_pose.poseStamped.pose.position.x)
+        << " " << std::to_string(car_pose.poseStamped.pose.position.y) << " " << std::to_string(car_pose.poseStamped.pose.position.z)
+        << " " << std::to_string(car_pose.poseStamped.pose.orientation.x) << " " << std::to_string(car_pose.poseStamped.pose.orientation.y)
+        << " " << std::to_string(car_pose.poseStamped.pose.orientation.z) << " " << std::to_string(car_pose.poseStamped.pose.orientation.w)
+        << std::endl;);
   
-  // {
-      //get current pose
-      iiwa_msgs::CartesianPose car_pose = pose_state.getPose();
-      std::cout << "Pose: " << car_pose.poseStamped.pose.orientation.w << std::endl;
+  ROS_INFO("get current pose is finish................. "); 
 
-      ROS_INFO_STREAM(
-        std::to_string(car_pose.poseStamped.pose.position.x)
-            << " " << std::to_string(car_pose.poseStamped.pose.position.y) << " " << std::to_string(car_pose.poseStamped.pose.position.z)
-            << " " << std::to_string(car_pose.poseStamped.pose.orientation.x) << " " << std::to_string(car_pose.poseStamped.pose.orientation.y)
-            << " " << std::to_string(car_pose.poseStamped.pose.orientation.z) << " " << std::to_string(car_pose.poseStamped.pose.orientation.w)
-            << std::endl;);
-      ros::Duration(0.1).sleep();
-
-      //send a new Cartesian pose
-      geometry_msgs::PoseStamped command_pose;
-      command_pose = car_pose.poseStamped;
-      command_pose.pose.position.x = car_pose.poseStamped.pose.position.x+direction_x_add;
-      command_pose.pose.position.y = car_pose.poseStamped.pose.position.y+direction_y_add;
-      command_pose.pose.position.z = car_pose.poseStamped.pose.position.z+direction_z_add;
-      command_pose.pose.orientation.x = car_pose.poseStamped.pose.orientation.x;
-      command_pose.pose.orientation.y = car_pose.poseStamped.pose.orientation.y;
-      command_pose.pose.orientation.z = car_pose.poseStamped.pose.orientation.z;
-      command_pose.pose.orientation.w = car_pose.poseStamped.pose.orientation.w;
-
-      pose_command.setPose(command_pose);
-
-      //  excute rotation command
-      double rotation_x_radian = rotation_x * 180/M_PI;
-      double rotation_y_radian = rotation_y * 180/M_PI;
-      double rotation_z_radian = rotation_z * 180/M_PI;
-
-      tf2::Quaternion q_orig,q_rot,q_new;
-
-      tf2::convert(command_pose.pose.orientation,q_orig);
-      q_rot.setRPY(rotation_x_radian,rotation_y_radian,rotation_z_radian);
-      // world frame
-      // q_new = q_rot*q_orig;
-      // TCP frame
-      q_new = q_orig*q_rot;      
-      q_new.normalize();
-
-      tf2::convert(q_new,command_pose.pose.orientation);
-
-      pose_command.setPose(command_pose);
+  ros::Duration(0.1).sleep();
 
 
-       
+  //send a new Cartesian pose
+  geometry_msgs::PoseStamped command_pose;
+  command_pose = car_pose.poseStamped;
+  command_pose.pose.position.x = car_pose.poseStamped.pose.position.x+direction_x_add;
+  command_pose.pose.position.y = car_pose.poseStamped.pose.position.y+direction_y_add;
+  command_pose.pose.position.z = car_pose.poseStamped.pose.position.z+direction_z_add;
+  command_pose.pose.orientation.x = car_pose.poseStamped.pose.orientation.x;
+  command_pose.pose.orientation.y = car_pose.poseStamped.pose.orientation.y;
+  command_pose.pose.orientation.z = car_pose.poseStamped.pose.orientation.z;
+  command_pose.pose.orientation.w = car_pose.poseStamped.pose.orientation.w;
 
-       
-  // }
+  pose_command.setPose(command_pose);
+
+
+  //  excute rotation command
+  double rotation_x_radian = rotation_x * 180/M_PI;
+  double rotation_y_radian = rotation_y * 180/M_PI;
+  double rotation_z_radian = rotation_z * 180/M_PI;
+
+  tf2::Quaternion q_orig,q_rot,q_new;
+
+  tf2::convert(command_pose.pose.orientation,q_orig);
+  q_rot.setRPY(rotation_x_radian,rotation_y_radian,rotation_z_radian);
+  // world frame
+  // q_new = q_rot*q_orig;
+  // TCP frame
+  q_new = q_orig*q_rot;      
+  q_new.normalize();
+
+  tf2::convert(q_new,command_pose.pose.orientation);
+
+  pose_command.setPose(command_pose);
+
+  ros::Duration(2).sleep();
 
   std::cerr << "Stopping spinner..." << std::endl;
   spinner.stop();
