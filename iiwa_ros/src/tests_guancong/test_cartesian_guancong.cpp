3e3db5020e7760b22f83d1cdac4428eb25d89baa
ClarenceLuo <clarenceluoguancong@gmail.com>
2019-10-14 15:50:10 +0800

    git

--- iiwa_ros/src/tests_guancong/test_cartesian_guancong.cpp
+++ iiwa_ros/src/tests_guancong/test_cartesian_guancong.cpp
@@ -20,20 +20,19 @@ static bool quit{false};
 
 void signalHandler(int /*unused*/) { quit = true; }
 
-// int main(int argc, char **argv)
 int main()
 {
+
   iiwa_ros::state::CartesianPose pose_state;
   iiwa_ros::command::CartesianPose  pose_command;
   pose_command.init("iiwa");  
   pose_state.init("iiwa"); // That is the namespace under which the topic lives.
 
   ROS_INFO("init is finish................. "); 
-  // get roslauch param
-  // ros::init(argc, argv, "test");
 
+
+  // get roslauch param
   ros::NodeHandle nh;
-  // ros::NodeHandle nh("~"); // private node handle
 
   double direction_x_add;
   double direction_y_add;
@@ -46,15 +45,12 @@ int main()
   nh.getParam("CartesianPoseState/direction_x_add", direction_x_add);
   nh.getParam("CartesianPoseState/direction_y_add", direction_y_add);
   nh.getParam("CartesianPoseState/direction_z_add", direction_z_add);
-  nh.getParam("rotation_x", rotation_x);
-  nh.getParam("rotation_y", rotation_y);
-  nh.getParam("rotation_z", rotation_z);
-  nh.getParam("command_cartisian_mode", command_cartisian_mode);
+  nh.getParam("CartesianPoseState/rotation_x", rotation_x);
+  nh.getParam("CartesianPoseState/rotation_y", rotation_y);
+  nh.getParam("CartesianPoseState/rotation_z", rotation_z);
+  nh.getParam("CartesianPoseState/command_cartisian_mode", command_cartisian_mode);
 
-  ROS_INFO("parameter is finish load.................%f ",direction_z_add);
-
-
-
+  ROS_INFO("parameter is finish load.................");
 
 
   // ROS spinner.
@@ -71,8 +67,8 @@ int main()
   ros::Duration(0.5).sleep();
 
 
+  // get crurent iiwa cartesian pose
   iiwa_msgs::CartesianPose car_pose = pose_state.getPose();
-  std::cout << "Pose: " << car_pose.poseStamped.pose.orientation.w << std::endl;
 
   ROS_INFO_STREAM(
     std::to_string(car_pose.poseStamped.pose.position.x)
@@ -105,22 +101,21 @@ int main()
   double rotation_y_radian = rotation_y * 180/M_PI;
   double rotation_z_radian = rotation_z * 180/M_PI;
 
-  
-
+  // Quaternian transform RPY to multiplication
   tf2::Quaternion q_orig,q_rot,q_new;
 
   tf2::convert(command_pose.pose.orientation,q_orig);
   q_rot.setRPY(rotation_x_radian,rotation_y_radian,rotation_z_radian);
-  // world frame
-  // q_new = q_rot*q_orig;
-  // TCP frame
-  q_new = q_orig*q_rot;      
+
+  // q_new = q_rot*q_orig;   // world frame
+  q_new = q_orig*q_rot;      // TCP frame
   q_new.normalize();
 
   tf2::convert(q_new,command_pose.pose.orientation);
 
   pose_command.setPose(command_pose);
 
+  // Wait a bit, so that you can be sure the command are sended.
   ros::Duration(2).sleep();
 
   while(!quit)
